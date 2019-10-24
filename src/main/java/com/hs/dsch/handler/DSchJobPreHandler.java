bab29bc7fb5dc5c268c8c5cdf5363146e0eab33d
gilbert.wang <gilbert.wang@vipshop.com>
2019-10-24 14:52:23 +0800

    1

--- src/main/java/com/hs/dsch/handler/DSchJobPreHandler.java
+++ src/main/java/com/hs/dsch/handler/DSchJobPreHandler.java
@@ -1,12 +1,23 @@
 package com.hs.dsch.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hs.dsch.conf.DSchConfiguration;
 import com.hs.dsch.conf.DSchContext;
 import com.hs.dsch.proto.DSchAdminProto.DSchJobStatus;
+import com.hs.dsch.util.HttpClient;
 
 public class DSchJobPreHandler implements DSchJobHandler {
+	
+	private DSchConfiguration dschConfiguration = DSchContext.getInstance().getDSchConfiguration();
+	private HttpClient httpClient = DSchContext.getInstance().getHttpClient();
+	
+	private static Logger logger = LoggerFactory.getLogger(DSchJobPreHandler.class);
 
 	@Override
 	public void handle(DSchJobContext context) {
+		logger.info("从服务端拉取命令:{}" , context);
 		
 		if (DSchContext.getInstance().getJobStatus(context.getJobId()) == DSchJobStatus.DSCH_JOB_ST_STOPPED_VALUE) {
 			// 当前任务状态为停止状态并且获取的命令为启动
